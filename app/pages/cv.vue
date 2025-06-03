9dd4b8ec43d0cd7314c120b6111a9741ec4e5dd7
asasinmode <skin9470@gmail.com>
2025-06-03 17:15:32 +0200

    feat: separate cv styles

--- app/pages/cv.vue
+++ app/pages/cv.vue
@@ -1,20 +1,9 @@
+<script setup lang="ts">
+useHead({ link: [{ rel: 'stylesheet', href: '/cv.css' }] });
+</script>
+
 <template>
 	<main id="cv">
 		hiiiiiii
 	</main>
 </template>
-
-<style>
-#cv {
-	background: white;
-	color: black;
-	width: 210mm;
-	height: 297mm;
-}
-
-@media print {
-	#language-switch {
-		display: none !important;
-	}
-}
-</style>
