77f529a5019458cf7d0e2ade7e1244133b63ebf6
asasinmode <skin9470@gmail.com>
2025-06-03 17:07:11 +0200

    feat: init cv page

---
+++ app/pages/cv.vue
@@ -0,0 +1,20 @@
+<template>
+	<main id="cv">
+		hiiiiiii
+	</main>
+</template>
+
+<style>
+#cv {
+	background: white;
+	color: black;
+	width: 210mm;
+	height: 297mm;
+}
+
+@media print {
+	#language-switch {
+		display: none !important;
+	}
+}
+</style>
