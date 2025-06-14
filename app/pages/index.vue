11476873436de2d9a53902e1905ad9a31f5c618c
asasinmode <skin9470@gmail.com>
2025-06-14 11:46:38 +0200

    feat(index): init stuff layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,7 +19,7 @@ const localePath = useLocalePath();
 			<li>programmer</li>
 			<li>i like coding</li>
 		</ul>
-		<h3 id="stuff-header">
+		<h2 id="stuff-header">
 			<span id="stuff-header-text">
 				<svg
 					id="stuff-header-rainbow"
@@ -51,41 +51,42 @@ const localePath = useLocalePath();
 				</svg>
 				stuff
 			</span>
-		</h3>
+		</h2>
 		<ul id="stuff-list">
 			<li>
-				<h5>fullstack blog cms</h5>
-				<p>featuring an html and css editor, custom file and image management with automatically generated responsive picture variants, seo tools and more</p>
+				<h3>fullstack blog cms</h3>
+				<p>featuring an html and css editor, custom file and image management with automatically generated responsive picture variants and seo tools</p>
 			</li>
 			<li>
-				<h5>a wheelbarrow</h5>
-				<p>vanilla friendly minecraft mod adding a wheelbarrow you can use to transport mobs around</p>
+				<h3>a wheelbarrow</h3>
+				<p>vanilla friendly minecraft mod adding a wheelbarrow that you can use to transport mobs around</p>
 			</li>
 			<li>
-				<h5>warframe offering prices scraper</h5>
-				<p>a small cli script for finding the most expensive warframe syndicate offering mods</p>
+				<h3>warframe offering prices scraper</h3>
+				<p>a small cli tool for listing and finding the most expensive warframe syndicate offerings</p>
 			</li>
 			<li>
-				<h5>league of legends damage calculator</h5>
-				<p>unfortunately not updated, but in its glory days it accurately showed the dps difference between different builds</p>
+				<h3>league of legends damage calculator</h3>
+				<p>unfortunately not updated, but in its glory days it accurately showed the damage difference between different builds</p>
 			</li>
 		</ul>
-		<p>
-			big or small, web, game or cli - doesn't matter. if it sounds fun or useful, i'll do it. more of the stuff I made can be found on the <NuxtLink :to="localePath('projects')">
-				projects page
-			</NuxtLink>
-		</p>
-		<p>
-			personally, i enjoy creating user-facing stuff that someone can interact with or creating complicated logic that make me feel smart<br>
-			despite that, i understand that not all of programming can be exciting and sometimes a job needs to get done. I'm here for those times too
-		</p>
-		<h3>experience</h3>
+		<div id="stuff-note">
+			<p>
+				big or small, web, game or cli - doesn't matter. if it sounds fun or useful, i'll do it. More of the stuff I made can be found on the <NuxtLink :to="localePath('projects')">
+					projects page
+				</NuxtLink>
+			</p>
+			<p>
+				personally, i enjoy creating user-facing stuff that someone can interact with or creating complicated logic that make me feel smart. Despite it, i understand that not all of programming can be exciting and sometimes a job needs to get done. I'm here for those times too
+			</p>
+		</div>
+		<h2>experience</h2>
 		<ol>
 			<li>4 years of polish <i>technikum</i> (vocational IT school), in the middle of which i started freelance work</li>
 			<li>internship that turned into 4 years of junior, then mid fullstack web developer position</li>
 			<li>? - excited for what future holds</li>
 		</ol>
-		<h5>web dev</h5>
+		<h3>web dev</h3>
 		<p>
 			if it's javascript, html or css related i will be fine, regardless of the framework and tools. That said, i prefer Vue and use it a lot with Nuxt and tailwind (unocss). I can handle a typescript backend, whether it's using an ORM or pure SQL/Mongo, and 3rd-party integrations. I have written tests with Jest then migrated them to Vitest and rewritten a codebase from cjs to esm
 		</p>
@@ -94,14 +95,14 @@ const localePath = useLocalePath();
 				Adrian Roselli's website
 			</NuxtLink>
 		</p>
-		<h5>not web dev</h5>
+		<h3>not web dev</h3>
 		<p>
 			my mindset is that given enough time i'll figure it out - sooner rather than later, it's all programming after all. I enjoy finding new ways of doing things and programming languages are really good at providing them. I know what Rust, Python and PHP look like and i feel like i wouldn't be <i>that</i> lost amidst Java decorators or Lua's 1-based indexing.
 		</p>
 		<p>
 			git for version control and a decent knowledge of what to do with branches, pull requests and commits. I'm familiar with creating/maintaining Github CI/CD pipelines, as well as making and using Docker containers. I use Neovim btw. Additionally i have experience with video editing
 		</p>
-		<h3>contact</h3>
+		<h2>contact</h2>
 		<p>
 			get in touch through email (<a href="mailto:stanislaw.perek.ui@gmail.com">stanislaw.perek.ui@gmail.com</a>) or a discord dm (username: <v-copiable-text text="asasinmode" />). Whether business related or something i yapped about on <strong>TODO blog</strong>, i'm happy to talk
 		</p>
