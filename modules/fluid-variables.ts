6a1d9d579e9a36271b40a93452909fa055d9978d
asasinmode <skin9470@gmail.com>
2025-11-20 19:46:39 +0100

    feat: update fluid variables module

--- modules/fluid-variables.ts
+++ modules/fluid-variables.ts
@@ -1,113 +1,159 @@
 import fs from 'node:fs/promises';
-import path from 'node:path';
-import process from 'node:process';
-import { addTemplate, defineNuxtModule, updateTemplates } from '@nuxt/kit';
-import { normalizePath } from 'vite';
+import { addTemplate, defineNuxtModule, resolveFiles, updateTemplates } from '@nuxt/kit';
 
-export default defineNuxtModule({
+interface IFluidVariablesConfig {
+	/**
+	 * default min viewport in px
+	 * @default 380
+	 */
+	minViewport?: number;
+	/**
+	 * default max viewport in px
+	 * @default 1280
+	 */
+	maxViewport?: number;
+	/**
+	 * should be the same as page's `:root { font-size }` property
+	 * note that it **doesn't apply to breakpoints**, just values, so with root's `font-size: 14px` `--fluid-f768-16-32-t1024` would mean at `768px` the value is `14px` and at `1024px` it grows to `28px`
+	 * @default 16;
+	 */
+	remInPx?: number;
+	/**
+	 * used for `variableRegexp` and created variable names. Should match the regexp if it's overriden
+	 * @default --fluid
+	 */
+	variablePrefix?: string;
+	/**
+	 * created based on the `variablePrefix` option, this overrides it
+	 * the regexp should return 4 matches where `[minViewport, sizeFrom, sizeTo, maxViewport]`
+	 * @default --fluid-(?:f(\\d+)-)?(\\d+)-(\\d+)(?:-t(\\d+))?
+	 */
+	variableRegexp?: RegExp;
+}
+
+export default defineNuxtModule<IFluidVariablesConfig>({
 	meta: {
-		name: 'asasinmode:fluid-variables',
+		name: 'fluid-variables',
+		configKey: 'fluidVariables',
 	},
-	async setup(_options, nuxt) {
+	async setup(options, nuxt) {
+		options.minViewport ??= 320;
+		options.maxViewport ??= 1440;
+		options.remInPx ??= 16;
+		options.variablePrefix ??= '--fluid';
+		options.variableRegexp ??= new RegExp(`${options.variablePrefix}-(?:f(\\d+)-)?(\\d+)-(\\d+)(?:-t(\\d+))?`, 'g');
+
 		const variablesByFile: Map<string, IFileVariables> = new Map();
 		let generatedCss = '';
 
-		const files = await findStyleFiles(path.join(process.cwd(), 'app'));
-		await Promise.all(files.map(file => processFile(file)));
-		generatedCss = generateCss(variablesByFile);
+		const files = await resolveFiles(nuxt.options.srcDir, ['**/*.css', '**/*.vue'], {
+			ignore: ['node_modules/**', '.nuxt/**', 'dist/**', '**/node_modules/**', '**/.git/**'],
+		});
 
-		const template = 'asasinmode:fluid-variables.css';
+		await Promise.all(files.map(file => processFile(file)));
+
+		generatedCss = generateCss(variablesByFile, options);
+
+		const template = 'fluid-variables.css';
 		addTemplate({
 			filename: template,
 			getContents: () => generatedCss,
 		});
 
-		async function processFile(file: string, isVue = file.endsWith('.vue')) {
-			if (!file.endsWith('assets/reset.css')) {
-				variablesByFile.set(file, await extractFluidVariables(file, isVue));
-			}
+		async function processFile(file: string) {
+			variablesByFile.set(file, await extractFluidVariables(file, options));
 		}
 
+		nuxt.options.css.push('#build/fluid-variables.css');
+
 		nuxt.hook('builder:watch', async (_event, path) => {
-			const isVue = path.endsWith('.vue');
-			if (path.endsWith('.css') || isVue) {
-				await processFile(path, isVue);
-				generatedCss = generateCss(variablesByFile);
+			if (path.endsWith('.css') || path.endsWith('.vue')) {
+				await processFile(path);
+				generatedCss = generateCss(variablesByFile, options);
 				updateTemplates({ filter: t => t.filename === template });
 			}
 		});
 	},
 });
 
-type IFileVariables = Map<string, [number, number]>;
+type IFileVariables = Map<string, IFluidVariable>;
 
-const variableRegex = /--fluid-(\d+)-(\d+)/g;
-const minScreenWidth = 20;
-const maxScreenWidth = 90;
-const remInPx = 16;
+interface IFluidVariable {
+	sizeFrom: number;
+	sizeTo: number;
+	fromViewport?: number;
+	toViewport?: number;
+}
 
-function generateClamp(sizeFrom: number, sizeTo: number): string {
-	sizeFrom = sizeFrom / remInPx;
-	sizeTo = sizeTo / remInPx;
+const defaultRemInPx = 16;
 
+function generateClamp(
+	sizeFrom: number,
+	sizeTo: number,
+	minScreenWidth: number,
+	maxScreenWidth: number,
+	remInPx: number,
+): string {
 	const slope = (sizeTo - sizeFrom) / (maxScreenWidth - minScreenWidth);
 	const yAxisIntersection = -minScreenWidth * slope + sizeFrom;
 
-	return `clamp(${sizeFrom}rem, ${yAxisIntersection.toFixed(3)}rem + ${(slope * 100).toFixed(3)}vw, ${sizeTo}rem)`;
+	return `clamp(${formatNumber(sizeFrom / defaultRemInPx)}rem, ${formatNumber(yAxisIntersection / remInPx)}rem + ${formatNumber(slope * 100)}vw, ${formatNumber(sizeTo / defaultRemInPx)}rem)`;
 }
 
-async function extractFluidVariables(filePath: string, isVue: boolean): Promise<IFileVariables> {
+function formatNumber(number: number) {
+	return Number.parseFloat(number.toFixed(4));
+}
+
+async function extractFluidVariables(filePath: string, config: IFluidVariablesConfig): Promise<IFileVariables> {
 	const variables: IFileVariables = new Map();
+	const content = await fs.readFile(filePath, 'utf-8');
 
-	let content = await fs.readFile(filePath, 'utf-8');
-	if (isVue) {
-		content = content.slice(content.indexOf('\n<style>') + 9, content.indexOf('\n</style>'));
-	}
+	for (const match of content.matchAll(config.variableRegexp!)) {
+		const fromViewport = match[1] ? Number(match[1]) : undefined;
+		const sizeFrom = Number(match[2]);
+		const sizeTo = Number(match[3]);
+		const toViewport = match[4] ? Number(match[4]) : undefined;
 
-	for (const match of content.matchAll(variableRegex)) {
-		const sizeFrom = Number(match[1]);
-		const sizeTo = Number(match[2]);
-		if (!Number.isNaN(sizeFrom) && !Number.isNaN(sizeTo) && !variables.has(`${sizeFrom}-${sizeTo}`)) {
-			variables.set(`${sizeFrom}-${sizeTo}`, [sizeFrom, sizeTo]);
+		const prefix = fromViewport ? `f${fromViewport}-` : '';
+		const suffix = toViewport ? `-t${toViewport}` : '';
+
+		if (
+			!Number.isNaN(sizeFrom)
+			&& !Number.isNaN(sizeTo)
+			&& !variables.has(`${prefix}${sizeFrom}-${sizeTo}${suffix}`)
+		) {
+			variables.set(`${prefix}${sizeFrom}-${sizeTo}${suffix}`, {
+				sizeFrom,
+				sizeTo,
+				fromViewport,
+				toViewport,
+			});
 		}
 	}
 
 	return variables;
 }
 
-function generateCss(variablesByFile: Map<string, IFileVariables>) {
+function generateCss(variablesByFile: Map<string, IFileVariables>, config: IFluidVariablesConfig) {
 	const wantedVariables: IFileVariables = new Map();
+
 	for (const fileVariables of variablesByFile.values()) {
-		for (const [variable, [sizeFrom, sizeTo]] of fileVariables.entries()) {
-			if (!wantedVariables.has(variable)) {
-				wantedVariables.set(variable, [sizeFrom, sizeTo]);
+		for (const [key, variable] of fileVariables.entries()) {
+			if (!wantedVariables.has(key)) {
+				wantedVariables.set(key, variable);
 			}
 		}
 	}
 
 	let css = ':root {\n';
-	for (const [variable, [minSize, maxSize]] of wantedVariables.entries()) {
-		css += `\t--fluid-${variable}: ${generateClamp(minSize, maxSize)};\n`;
+
+	for (const [key, { sizeFrom, sizeTo, fromViewport, toViewport }] of wantedVariables.entries()) {
+		css += `    ${config.variablePrefix!}-${key}: ${generateClamp(sizeFrom, sizeTo, fromViewport || config.minViewport!, toViewport || config.maxViewport!, config.remInPx!)};\n`;
 	}
+
 	css += '}';
 
 	return css;
-}
-
-async function findStyleFiles(dir: string, fileList: string[] = []): Promise<string[]> {
-	const entries = await fs.readdir(dir, { withFileTypes: true });
-	for (const entry of entries) {
-		const fullPath = path.join(dir, entry.name);
-		if (entry.isFile()) {
-			const ext = path.extname(entry.name);
-			if (ext === '.css' || ext === '.vue') {
-				fileList.push(normalizePath(fullPath));
-			}
-		} else if (entry.name !== 'node_modules') {
-			await findStyleFiles(fullPath, fileList);
-		}
-	}
-	return fileList;
 }
 
 // vite plugin maybe useful for just vue apps
